e92102edf7c2bbf7aa1d68266cf9e20345789a01
bee7er <contact_bee@yahoo.com>
2020-05-09 16:21:44 +0100

    Make card headers teal to match front end

--- public/js/app.js
+++ public/js/app.js
@@ -37393,8 +37393,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/vagrant/Code/sorted/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/vagrant/Code/sorted/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/brianetheridge/Code/sorted/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/brianetheridge/Code/sorted/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
